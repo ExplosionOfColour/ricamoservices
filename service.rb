9a5e2d3d9b1399033f404ba297f531928d6ec5d8
ExplosionOfColour <bex_3000@hotmail.com>
2012-11-02 15:46:27 +0000

    Initial commit.

---
+++ service.rb
@@ -0,0 +1,15 @@
+require 'json'
+require 'sinatra'
+
+chief_complaints = [
+  { name: "Dizziness" },
+  { name: "Chest Pain" },
+]
+
+before /.*/ do
+  content_type :json
+end
+
+get '/chief_complaints' do
+  chief_complaints.to_json
+end
